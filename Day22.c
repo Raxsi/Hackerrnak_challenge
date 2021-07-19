0e3a129ab362d579d9ae90ec9e463eec2bac085f
raxsi <shenoyrakshith96@gmail.com>
2021-07-19 21:03:48 +0530

    initial commit

---
+++ Day22.c
@@ -0,0 +1,96 @@
+/*
+Day 21: Binary Tree
+
+Input Format
+
+The locked stub code in your editor reads the following inputs and assembles them into a binary search tree:
+The first line contains an integer, , denoting the number of nodes in the tree.
+Each of the  subsequent lines contains an integer, , denoting the value of an element that must be added to the BST.
+
+Output Format
+
+The locked stub code in your editor will print the integer returned by your getHeight function denoting the height of the BST.
+
+Sample Input
+
+7
+3
+5
+2
+1
+4
+6
+7
+Sample Output
+
+3
+*/
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+          //Write your code here
+          if(!root){
+            return -1;}
+        int leftDepth = getHeight(root->left)+1;
+        int rightDepth = getHeight(root->right)+1;
+        return leftDepth>rightDepth?leftDepth:rightDepth;
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
+    
+       
+            
+        
